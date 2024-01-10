53e5085b94c7154e405b98df5977e39f65f91c6f
Gaurav Dhiman <imgauravdhiman@gmail.com>
2024-01-10 19:23:22 +0530

    changes made by gaurav added base layer

--- poll_stations/admin.py
+++ poll_stations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Stations
 # Register your models here.
+
+class StationsAdmin(admin.ModelAdmin):
+    point_zoom =1
+
+admin.site.register(Stations, StationsAdmin)
--- poll_stations/views.py
+++ poll_stations/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Stations
+from django.core.serializers import serialize
 def stations(request)->HttpResponse:
     """
     this is demo function
     """
-    return render(request=request, template_name='poll_stations/map.html')
+    station = serialize("geojson", Stations.objects.all()[:10])
+    print(station)
+    context = {'station':station}
+    return render(request=request, template_name='poll_stations/map.html', context=context)
